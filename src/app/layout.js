3c0df01b68e92ad5f62f7e68e81b9990909c9e9e
luizHenrique05 <henrique.luiz-sup@univem.edu.br>
2024-08-03 12:30:28 -0300

    chore: remove teste

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <div>teste</div>
       <body className={inter.className}>{children}</body>
     </html>
   );
