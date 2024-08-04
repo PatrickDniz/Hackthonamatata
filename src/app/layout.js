fd047ae528f01c4694ed103cf48b598d956866c9
luizHenrique05 <henrique.luiz-sup@univem.edu.br>
2024-08-04 14:25:16 -0300

    chore: adding metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,17 @@ import Footer from "@/components/Footer";
 
 const lato = Lato({  weight: ['400', '700'], subsets: ["latin"] });
 
+export const metadata = {
+  title: 'A maior plataforma humanitária do Brasil - Braza',
+  description: 'Ajude em campanhas, seja um voluntário ou disponibilize seu imóvel parado para auxiliar quem precisa. Sempre podemos ajudar com alguma coisa.',
+  keywords: 'Braza, Voluntario, Voluntaria, Campanhas, Crises Climáticas',
+  icons: {
+    icon: ['/favicon/favicon.ico?v=4'],
+    apple: ['/favicon/apple-touch-icon.png?v=4'],
+    shortcut: ['/favicon/apple-touch-icon.png']
+  }
+}
+
 export default function RootLayout({ children }) {
  
   return (
